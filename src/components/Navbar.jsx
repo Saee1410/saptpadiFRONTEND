94fb5b90dad2fdad708f8ec8a4e24c320c984a5e
Saee1410 <saivijaybhosale@gmail.com>
2026-05-01 13:44:28 +0530

    edidt the navbar origin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, useNavigate, useState } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
